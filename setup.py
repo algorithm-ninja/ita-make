0148ec95a7de0a868601d172453857471dfb73a7
William Di Luigi <williamdiluigi@gmail.com>
2016-05-31 11:27:40 +0200

    Include cmscommon as an external module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='itamake',
-      version='0.1.1',
+      version='0.2.0',
       description='A stripped down cmsMake which *only* makes, nothing more.',
       author='Algorithm Ninja',
       author_email='algorithm@ninja',
@@ -12,7 +12,8 @@ setup(name='itamake',
       packages=find_packages(),
       install_requires=[
           'pyyaml',
-          'six'
+          'six',
+          'cmscommon==0.1.0',
       ],
       entry_points={
           'console_scripts': [
